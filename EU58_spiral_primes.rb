0e1fe7e3788bd2ee60d72431aebca12777ae3b20
dperel <davidperel.pdf@gmail.com>
2015-09-02 11:30:17 -0400

    Problem 58 solved

--- EU58_spiral_primes.rb
+++ EU58_spiral_primes.rb
@@ -12,8 +12,6 @@
 
 # If one complete new layer is wrapped around the spiral above, a square spiral with side length 9 will be formed. If this process is continued, what is the side length of the square spiral for which the ratio of primes along both diagonals first falls below 10%?
 
-# n = index of outermost layer. Square above is a square where n = 4
-require 'pry'
 class Spiral_Primes
 @@ratio = 1
 
